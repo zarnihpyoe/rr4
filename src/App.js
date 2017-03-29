aae495246bb29b8588085971c9aa7446b07ddfae
Zarni Phyo <zarniphyo95@gmail.com>
2017-03-30 02:02:02 +0630

    lesson2: Use Route with different styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Route,
+ } from 'react-router-dom'
+
+const Home = (props) => {
+  /* to checkout what's inside props which got passed in when the route calls the component */
+  console.log(props)
+  return (<h1>Home</h1>)
+}
 
 const App = () => (
   <Router>
-    <h1>Hello, World</h1>
+    <div>
+      <Route exact path='/' component={Home} />
+      <Route path='/about' render={() => <h1>About</h1>} />
+      <Route
+        path='/contact'
+        children={({match}) => match && <h1>Contact</h1>} />
+    </div>
   </Router>
 )
 
