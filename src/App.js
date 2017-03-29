e93a40eaf61623ffd8eb811a6d4705249c05e3ca
Zarni Phyo <zarniphyo95@gmail.com>
2017-03-30 01:48:59 +0630

    lesson1: Make react-router-dom up and running

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+const App = () => (
+  <Router>
+    <h1>Hello, World</h1>
+  </Router>
+)
+
+export default App
