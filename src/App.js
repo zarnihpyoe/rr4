55757a5f43314262a17ce278e5398d4016c18572
Zarni Phyo <zarniphyo95@gmail.com>
2017-03-30 21:09:02 +0630

    lesson12: Use Redirect to redirect base on logged in state

--- src/App.js
+++ src/App.js
@@ -4,31 +4,39 @@ import {
   Route,
   Link,
   Switch,
+  Redirect,
 } from 'react-router-dom'
 
 import './App.css'
 
-const Home = () => (<h1>Home</h1>)
-const Menu = () => (
-  <div>
-    <h1>Menu</h1>
-    <Link to='/menu/food'>Food</Link>
-    <Link to='/menu/drinks'>Drinks</Link>
-    <Link to='/menu/sides'>Sides</Link>
-    <Route
-      path='/menu/:section'
-      render={({match}) => (<h2>{match.params.section}</h2>)} />
-  </div>
+const loggedIn = false
+
+const Links = () => (
+  <nav>
+    <Link to='/'>Home</Link>
+    <Link to='/old/123'>Old</Link>
+    <Link to='/new/456'>New</Link>
+    <Link to='/protected'>Protected</Link>
+  </nav>
 )
 
 const App = () => (
   <Router>
     <div>
-      <Link to='/'>Home</Link>
-      <Link to='/menu'>Menu</Link>
+      <Links />
+      <Route exact path='/' render={() => (<h1>Home</h1>)} />
+      <Route path='/new/:str' render={({match}) => (<h1>New: {match.params.str}</h1>)} />
+      {/** showing how params of original url is carried over to redirected url **/}
+      <Route path='/old/:str' render={({match}) => (
+          <Redirect to={`/new/${match.params.str}`} />
+        )} />
 
-      <Route exact path='/' component={Home} />
-      <Route path='/menu' component={Menu} />
+      {/** protecting a route with permission, use with login page and user landing page **/}
+      <Route path='/protected' render={() => (
+          loggedIn
+          ? <h1>Welcome to the protected page!</h1>
+          : <Redirect to='/new/login' />
+        )} />
     </div>
   </Router>
 )
